90cba6234271ae71ff3abfbf3904d8a60a2c3b33
LLCardenas-117 <lcardenas@student.neumont.edu>
2026-02-20 18:54:31 -0700

    Ahhhhhh. push it.

--- D&DNoteTaker/Program.cs
+++ D&DNoteTaker/Program.cs
@@ -1,4 +1,7 @@
 using D_DNoteTaker.Components;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,5 +26,21 @@ app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+
+
+
+//var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI");
+var connectionString = "mongodb+srv://lcardenas:Walnut1978@user.xummolz.mongodb.net/?appName=User";
+if (connectionString == null)
+{
+    Console.WriteLine("You must set your 'MONGODB_URI' environment variable. To learn how to set it, see https://www.mongodb.com/docs/drivers/csharp/current/get-started/create-connection-string");
+    Environment.Exit(0);
+}
+var client = new MongoClient(connectionString);
+var collection = client.GetDatabase("sample_mflix").GetCollection<BsonDocument>("movies");
+var filter = Builders<BsonDocument>.Filter.Eq("title", "Back to the Future");
+var document = collection.Find(filter).First();
+Console.WriteLine(document.ToJson(new JsonWriterSettings { Indent = true }));
 
 app.Run();
