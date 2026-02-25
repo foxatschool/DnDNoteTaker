cda10502eedc2eb04ce219fbb87fef332f83fa77
ScareCellar <98201877+ScareCellar@users.noreply.github.com>
2026-02-24 19:12:59 -0700

    textbox

---
+++ D&DNoteTaker/Components/Models/TextBox.cs
@@ -0,0 +1,14 @@
+﻿namespace D_DNoteTaker.Components.Models
+{
+    public class TextBox
+    {
+        string? name;
+        string text;
+
+        public TextBox(string? name = null)
+        {
+            this.name = name;
+            text = "";
+        }
+    }
+}
