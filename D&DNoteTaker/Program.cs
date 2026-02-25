36e36af736fa2c90e0e9226fa6676793982bdcef
ScareCellar <98201877+ScareCellar@users.noreply.github.com>
2026-02-24 17:20:33 -0700

    Revert "nothing changed"
    This reverts commit f79ddf36bdf2099b1253d7a2c73934aa9fe1c076.

--- D&DNoteTaker/Program.cs
+++ D&DNoteTaker/Program.cs
@@ -28,6 +28,8 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 
+
+
 //var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI");
 var connectionString = "mongodb+srv://lcardenas:Walnut1978@user.xummolz.mongodb.net/?appName=User";
 if (connectionString == null)
