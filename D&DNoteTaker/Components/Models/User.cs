e74edfee8677893fcd409d7f117a8c7dd7597bf8
LLCardenas-117 <lcardenas@student.neumont.edu>
2026-03-03 18:18:57 -0700

    Created Users

---
+++ D&DNoteTaker/Components/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using D_DNoteTaker.Data.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace D_DNoteTaker.Components.Models
+{
+    public class User : IDocument
+    {
+        public string Id { get; set; }
+
+        [Required (ErrorMessage = "You need a username")]
+        public string UserName { get; set; }
+
+        [Required (ErrorMessage = "You need a password")]
+        public string Password { get; set; }
+    }
+}
