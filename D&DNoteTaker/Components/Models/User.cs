4c806d314110aece9ff10fbaa42777e5a287212f
LLCardenas-117 <lcardenas@student.neumont.edu>
2026-03-06 19:27:56 -0700

    Created SignUp and LogIn page

--- D&DNoteTaker/Components/Models/User.cs
+++ D&DNoteTaker/Components/Models/User.cs
@@ -5,10 +5,14 @@ namespace D_DNoteTaker.Components.Models
 {
     public class User : Document
     {
-        [Required (ErrorMessage = "You need a username")]
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Usernames must be between 2 - 20 characters long")]
         public string UserName { get; set; }
 
-        [Required (ErrorMessage = "You need a password")]
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 8, ErrorMessage = "Passwords must be at least 8 characters long")]
         public string Password { get; set; }
+
+        public string[]? Campaigns { get; set; }
     }
 }
