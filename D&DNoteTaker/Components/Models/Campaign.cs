de8aacb5f627fccd9bea82721381ca3f7b84a1ad
ScareCellar <98201877+ScareCellar@users.noreply.github.com>
2026-03-11 18:15:54 -0600

    Worldbuilding works

--- D&DNoteTaker/Components/Models/Campaign.cs
+++ D&DNoteTaker/Components/Models/Campaign.cs
@@ -5,8 +5,8 @@ namespace D_DNoteTaker.Components.Models
 {
     public class Campaign : Document
     {
-        [Required]
-        public string Id { get; set; }
+        //[Required]
+        //public string Id { get; set; }
         [Required]
         public string Name { get; set; }
         [Required]
